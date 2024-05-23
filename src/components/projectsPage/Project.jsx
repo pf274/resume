9bdfd61199bcd55974c4c587571ce5564d53f9af
pf274 <peterfullmer33@gmail.com>
2024-05-23 16:56:11 -0600

    Extra items in project showcase

--- src/components/projectsPage/Project.jsx
+++ src/components/projectsPage/Project.jsx
@@ -1,15 +1,48 @@
 import { Typography } from "@mui/material";
 import { ResumeAccordion } from "../ResumeAccordion";
 
-export function Project({info}) {
+export function Project({ info }) {
   return (
-    <ResumeAccordion title={info.name} subtitle={info.description} startDateString={info.startDate} endDateString={info.endDate}>
+    <ResumeAccordion
+      title={info.name}
+      subtitle={info.description}
+      startDateString={info.startDate}
+      endDateString={info.endDate}
+    >
       <div>
-        {info.highlights && info.highlights.length > 0 && <ul>{info.highlights.map((highlight, index) => 
-            <li key={index}>{highlight}</li>
-          )}</ul>}
-        {info.url && <Typography><a href={info.url}>See Project</a></Typography>}
+        {info.highlights && info.highlights.length > 0 && (
+          <ul>
+            {info.highlights.map((highlight, index) => (
+              <li key={index}>{highlight}</li>
+            ))}
+          </ul>
+        )}
+        {info.showcase && info.showcase.length > 0 && (
+          <div>
+            <Typography variant="h6">Other Links</Typography>
+            <ul>
+              {info.showcase.map((url) => {
+                // const isYoutubeLink = url.includes("youtube.com");
+                // if (isYoutubeLink) {
+                //   return <iframe src={url} title={info.name} />;
+                // }
+                return (
+                  <li>
+                    <Typography>
+                      <a href={url}>{url}</a>
+                    </Typography>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        )}
+        {info.url && (
+          <Typography>
+            <a href={info.url}>See Project</a>
+          </Typography>
+        )}
       </div>
     </ResumeAccordion>
-  )
+  );
 }
